05e63e2592dbb0dc99defc462d92abb367075300
leonardo <23216886@aluno.univesp.br>
2025-04-10 12:11:51 -0300

    alteração da barra principal

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,17 +1,24 @@
-// src/components/HeroSection.jsx
 import React from 'react';
-import heroImage from '../assets/hero-image.webp'; // Importe a imagem
-import './HeroSection.css'; // Importe o arquivo CSS
+import './HeroSection.css';
+import heroImageSmall from '../assets/hero-image-small.webp';
+import heroImageMedium from '../assets/hero-image-medium.webp';
+import heroImageLarge from '../assets/hero-image-large.webp';
 
 function HeroSection() {
   return (
     <section className="hero-section">
-      <img src={heroImage} alt="Pessoas praticando Pilates" />
-      <div className="hero-text">
+      <div className="hero-content">
         <h1>Transforme seu corpo e mente com Pilates</h1>
         <p>Descubra os benefícios do Pilates para flexibilidade, força, postura e bem-estar geral.</p>
         <button>Agende sua aula experimental</button>
       </div>
+      <img
+        src={heroImageLarge} // Usar a imagem grande como src padrão
+        srcset={`${heroImageSmall} 480w, ${heroImageMedium} 768w, ${heroImageLarge} 1024w`}
+        sizes="(max-width: 480px) 480px, (max-width: 768px) 768px, 1024px"
+        alt="Pessoas praticando Pilates"
+        loading="lazy" // Adicione este atributo para carregar a imagem de forma lazy
+      />
     </section>
   );
 }
